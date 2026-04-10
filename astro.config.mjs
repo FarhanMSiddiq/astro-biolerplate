68f361b8e1982253e16b5dbbb47b402b1fb32673
farhan <farhanmaulanasiddiq@gmail.com>
2026-04-10 14:48:40 +0700

    chore: astojs netlify depedencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "static",
-  adapter: node({ mode: "standalone" }),
+  output: "server",
+  // adapter: node({ mode: "standalone" }),
+  adapter: netlify(),
 });
